87d58e826102627a9eb9bc42cc3f35bb97ad1053
Max Jhones <maxjhonesdasilva@gmail.com>
2024-07-11 22:53:29 -0300

    added

---
+++ Pedra/src/style/index.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle'
+ *{
+   margin:0;
+   padding:0;
+   box-sizing:border-box;
+   text-decoration:none;
+   border:none;
+   outline:none;
+   transition: 0.3s ease;
+   font-family: 'Poppins', sans-serif;
+'}
